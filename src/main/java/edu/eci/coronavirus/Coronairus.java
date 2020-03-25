3035530d4f5ba24c3accdc80f847684199a8bf8e
johann.paez <johann.paez@mail.escuelaing.edu.co>
2020-03-25 07:15:50 -0500

    Paquetes Y Pom

---
+++ src/main/java/edu/eci/coronavirus/Coronairus.java
@@ -0,0 +1,16 @@
+package edu.eci.coronavirus;
+
+/**
+ * Hello world!
+ *
+ */
+
+
+public class Coronairus {
+	
+	
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
